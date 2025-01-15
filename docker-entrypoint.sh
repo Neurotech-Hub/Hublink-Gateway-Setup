a394f8450b56fa8acbbea46a54c09015715759a7
Matt Gaidica, Ph.D. <matt@gaidi.ca>
2025-01-15 15:03:18 -0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,10 +5,5 @@ mkdir -p /media/hublink-usb
 chown 1000:1000 /media/hublink-usb
 chmod 777 /media/hublink-usb
 
-# Create data directory
-mkdir -p /media/hublink-usb/data
-chown 1000:1000 /media/hublink-usb/data
-chmod 777 /media/hublink-usb/data
-
 # Execute the main container command
 exec "$@" 
