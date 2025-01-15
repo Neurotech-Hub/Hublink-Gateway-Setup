0616a27dc3d924f533f0158dff0cff0894bed42f
Matt Gaidica, Ph.D. <matt@gaidi.ca>
2025-01-15 15:21:59 -0600

    entry point issue

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create and set permissions for mount points
-mkdir -p /media/hublink-usb
-chown 1000:1000 /media/hublink-usb
-chmod 777 /media/hublink-usb
-
-# Execute the main container command
-exec "$@" 
