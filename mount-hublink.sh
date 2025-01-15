4bd53ad48ce97dc6ad761fb32dfb5eac679f4334
Matt Gaidica, Ph.D. <matt@gaidi.ca>
2025-01-15 13:48:17 -0600

    Update mount-hublink.sh

--- mount-hublink.sh
+++ mount-hublink.sh
@@ -82,12 +82,12 @@ CURRENT_GID=$(id -g "$CURRENT_USER")
 logger "Using user $CURRENT_USER (UID:$CURRENT_UID GID:$CURRENT_GID) for mount"
 
 # Set mount options specifically for FAT32
-MOUNT_OPTS="rw,uid=$CURRENT_UID,gid=$CURRENT_GID,umask=000,dmask=000,fmask=000"
+MOUNT_OPTS="rw,users,uid=$CURRENT_UID,gid=$CURRENT_GID,umask=000,dmask=000,fmask=000"
 
-logger "Attempting mount with command: /bin/mount -t vfat -o $MOUNT_OPTS $DEVNAME ${REMOVEABLE_STORAGE_PATH}"
+logger "Attempting mount with command: /usr/sbin/mount.vfat -o $MOUNT_OPTS $DEVNAME ${REMOVEABLE_STORAGE_PATH}"
 
 # Try mounting with vfat filesystem type
-/bin/mount -t vfat -o "$MOUNT_OPTS" "$DEVNAME" "${REMOVEABLE_STORAGE_PATH}" 2>&1 | logger
+/usr/sbin/mount.vfat -o "$MOUNT_OPTS" "$DEVNAME" "${REMOVEABLE_STORAGE_PATH}" 2>&1 | logger
 MOUNT_STATUS=${PIPESTATUS[0]}
 
 if [ $MOUNT_STATUS -eq 0 ]; then
